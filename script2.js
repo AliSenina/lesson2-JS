273114e86d8d1dcc1e4e300e8d03464a0b75d47a
AliSenina <122289718+AliSenina@users.noreply.github.com>
2023-02-03 01:56:46 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,130 @@
+// let count=0;
+// let ask=prompt('Найбільша кількість живих організмів зосереджена')
+// if(ask==='біля земної поверхні'){
+//     count++
+// }
+// let ask2=prompt("Перші живі організми на Землі з'явилися")
+// if(ask2==='3,5 млрд років тому'){
+//     count++
+// }
+// let ask3=prompt('Різноманітніший видовий склад характерний для світу')
+// if(ask3==='тваринного'){
+//     count++
+// }
+// let ask4=prompt('Органічну речовину з компонентів неживої природи створюють')
+// if(ask4==='Рослини'){
+//     count++
+// }
+// let ask5=prompt('В утворенні грунту не бере участь')
+// if(ask5==='гірський льодовик'){
+//     count++
+// }
+// let ask6=prompt('До складу природного комплексу не входить')
+// if(ask6==='верхня мантія'){
+//     count++
+// }
+// let ask7=prompt('На поширення рослинного та тваринного світу на Землі найбільший вплив робить')
+// if(ask7==='клімат'){
+//     count++
+// }
+// let ask8=prompt('Як правило, нічний спосіб життя ведуть тварини')
+// if(ask8==='тропічної пустелі'){
+//     count++
+// }
+// let ask9=prompt('Як правило, мають хороший зір та швидко бігають тварини')
+// if(ask9==='савани'){
+//     count++
+// }
+// let ask10=prompt('Великі сухопутні тварини практично відсутні, інші мешкають переважно на деревах в природній зоні')
+// if(ask10==='вологих вічнозелених екваторіальних лісів'){
+//     count++
+// }
+// document.write(`${count} правильних відповідей з 10`)
+
+// if (count<=3){
+//     alert(`погано, ${count}`)
+
+// }
+//  else if (count<=6){
+//     alert(`добре, ${count}`)
+
+
+// }
+//  else if (count<=10){
+//     alert('відмінно', {count})
+
+// }
+
+//Завдання 2
+
+// let login = prompt('Введіть логін')
+// if (login==='ALINA') {
+//     let password = prompt('Введіть пароль');
+//     if (password === 'LOGOS') {
+//         alert(`Ласкаво просимо!`)
+//     }
+//     else if (password===null) {
+//         alert(`Вхід скасований`)
+//     }
+//     else {
+//         alert(`Пароль невірний`)
+//     }
+
+// }
+// else if (login===null) {
+//     alert(`Вхід скасований`);
+// }
+// else {
+//     alert(`Я вас не знаю`);
+// }
+
+//Завдання 3
+
+const month = prompt('Please put some month number')
+
+if(month == 1){
+    alert("January")
+  
+    
+}
+
+if(month == 2){
+    alert("February")
+}
+
+if(month==3){
+    alert("March")
+}
+
+if(month==4){
+    alert("April")
+}
+if(month==5){
+    alert("May")
+}
+if(month==6){
+    alert("June")
+}
+if(month==7){
+    alert("July")
+}
+if(month==8){
+    alert("August")
+}
+if(month==9){
+    alert("September")
+}
+if(month==10){
+    alert("October")
+}
+if(month==11){
+    alert("November")
+
+}
+if(month==12){
+    alert("December")
+    
+}
+else{
+    alert("I don't know")
+}
